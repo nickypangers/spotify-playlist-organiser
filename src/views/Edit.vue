34290cd41710ed2c4766459522d90e624b65d001
Nixon <nixon.p@mosaicdigi.com>
2021-07-30 18:15:31 +0800

    edit page back to playlist

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -1,12 +1,18 @@
 <template>
-  {{ selectedPlaylist }}
-  <br />
-  {{ playlistItemList }}
+  <div>
+    <button @click="goToPlaylist()">Back to Playlist</button>
+  </div>
+  <div>
+    {{ selectedPlaylist }}
+    <br />
+    {{ playlistItemList }}
+  </div>
 </template>
 
 <script>
 import { ref, computed, onMounted } from "vue";
 import { useStore } from "vuex";
+import { useRouter } from "vue-router";
 
 import API from "@/helpers/api";
 
@@ -14,11 +20,16 @@ export default {
   name: "Edit",
   setup() {
     const store = useStore();
+    const router = useRouter();
 
     const playlistItemList = ref([]);
 
     const selectedPlaylist = computed(() => store.state.playlist);
     const accessToken = computed(() => store.state.accessToken);
+
+    const playlistItemLength = computed(
+      () => selectedPlaylist.value.tracks.total
+    );
 
     async function getPlaylistItemList() {
       let response = await API.getPlaylistItemList({
@@ -34,8 +45,15 @@ export default {
       playlistItemList.value = response.data;
     }
 
+    function goToPlaylist() {
+      router.push({
+        name: 'Playlist',
+      });
+    }
+
     onMounted(async () => {
       //   console.log(selectedPlaylist.value);
+      console.log(playlistItemLength.value);
       await getPlaylistItemList();
       console.log(playlistItemList.value);
     });
@@ -43,6 +61,7 @@ export default {
     return {
       selectedPlaylist: selectedPlaylist,
       playlistItemList: playlistItemList,
+      goToPlaylist: goToPlaylist,
     };
   },
 };
